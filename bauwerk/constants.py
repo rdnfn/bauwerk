e0ff8c19d11c261f1b0a8b1667716770cc4a6028
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-13 23:34:31 +0000

    feat: improve game

--- bauwerk/constants.py
+++ bauwerk/constants.py
@@ -1,7 +1,8 @@
 """Module containing constants."""
 import os
+import pathlib
 
 # Note: constants should be UPPER_CASE
 constants_path = os.path.realpath(__file__)
-SRC_PATH = os.path.dirname(constants_path)
-PROJECT_PATH = os.path.dirname(SRC_PATH)
+SRC_PATH = pathlib.Path(constants_path)
+PROJECT_PATH = SRC_PATH.parent
--- bauwerk/widget/core.py
+++ bauwerk/widget/core.py
@@ -2,11 +2,14 @@
 
 import ipywidgets as widgets
 import bauwerk
+from bauwerk.constants import PROJECT_PATH
 import gym
+import matplotlib.pyplot as plt
 
 import numpy as np
 
 bauwerk.setup()
+plt.set_loglevel("error")
 
 
 class Game(widgets.HBox):
@@ -23,19 +26,31 @@ class Game(widgets.HBox):
         action_low = self.env.action_space.low[0]
 
         self.control = widgets.FloatSlider(
+            description="Battery",
             orientation="vertical",
             min=action_low,
             max=action_high,
             continuous_update=False,
+            layout={"height": "300px"},
         )
         self.control.observe(self.step, names="value")
-        self.out = widgets.Output(layout={"border": "1px solid black"})
+        self.vis = widgets.Output(layout={"width": "600px", "height": "300px"})
+        self.out = widgets.Output(
+            layout={
+                # "border": "1px solid black",
+                "width": "100px",
+                "height": "100px",
+            }
+        )
+        self.hidden_out = widgets.Output()
 
         with self.out:
             print("Get ready!")
 
-        children = [self.control, self.out]
+        children = [self.control, self.vis, self.out]
         super().__init__(children=children)
+
+        self.img_house = plt.imread(PROJECT_PATH / "widget/house.png")
 
         self.game_finished = False
         self.reset()
@@ -43,10 +58,35 @@ class Game(widgets.HBox):
     def reset(self):
 
         obs = self.env.reset()
-        self.obs = [obs.values]
+        self.obs_values = {
+            key: [np.array([0], dtype=np.float32)]
+            for key in obs.keys()
+            if key != "time_step"
+        }
+        self.add_obs(obs)
+        self.plot()
+
+    def add_obs(self, obs):
+        for key in self.obs_values.keys():
+            self.obs_values[key].append(obs[key])
 
     def plot(self):
-        pass
+
+        with plt.xkcd(scale=1, length=200, randomness=2):
+            self.vis.clear_output(wait=True)
+            with self.vis:
+                fig = plt.figure(constrained_layout=True, figsize=(8, 4))
+                subfigs = fig.subfigures(1, 2, wspace=0.07, width_ratios=[1, 2])
+                ax_left = subfigs[0].subplots(1)
+                ax_left.axis("off")
+                ax_left.imshow(self.img_house)
+                axs = subfigs[1].subplots(len(self.obs_values))
+                for i, (obs_name, obs_part) in enumerate(self.obs_values.items()):
+                    axs[i].plot(obs_part)
+                    axs[i].set_title(obs_name.replace("_", " "))
+                for ax in axs:
+                    ax.label_outer()
+                plt.show()
 
     def step(self, change):
         # pylint: disable=unused-argument
@@ -56,16 +96,20 @@ class Game(widgets.HBox):
             self.out.clear_output()
 
             action = self.control.value
-            with self.out:
-                print("Action", action)
+            # with self.out:
+            # print("Action", action)
             action = np.array([action], dtype=np.float32)
             # pylint: disable=unused-variable
             observation, reward, terminated, truncated, info = self.env.step(action)
 
+            self.add_obs(observation)
+
             with self.out:
-                print(observation)
+                # print(observation)
 
                 if terminated or truncated:
                     self.game_finished = True
                     self.control.set_trait("disabled", True)
                     print("Game over.")
+
+            self.plot()
