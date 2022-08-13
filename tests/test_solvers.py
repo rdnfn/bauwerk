95b4703cce7ba9e85c8b4975f805f6b0ddd461a2
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-13 13:04:22 +0000

    test: add tests for solver

---
+++ tests/test_solvers.py
@@ -0,0 +1,13 @@
+"""Test for CVXPY based env solvers."""
+
+import bauwerk
+import bauwerk.envs.solvers
+import gym
+
+
+def test_solar_battery_house_solver() -> None:
+    """Basic test whether running solver throws errors."""
+
+    bauwerk.setup()
+    env = gym.make("bauwerk/SolarBatteryHouse-v0")
+    bauwerk.envs.solvers.solve_solar_battery_house(env)
