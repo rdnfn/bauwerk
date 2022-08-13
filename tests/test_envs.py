0d1d964b98373d9b2cd91bb3db31a8c295c13ecc
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-13 16:37:17 +0000

    test: update to new step api

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -6,7 +6,7 @@ import gym
 
 def test_solar_battery_house():
     bauwerk.setup()
-    env = gym.make("bauwerk/SolarBatteryHouse-v0")
+    env = gym.make("bauwerk/SolarBatteryHouse-v0", new_step_api=True)
 
     env.reset()
     env.step(env.action_space.sample())
