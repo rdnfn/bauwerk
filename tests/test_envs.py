5d1f4a9d7fd4d9251e5d41fef9fcc62d65a0e28e
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-24 17:52:42 +0000

    docs: add test descriptions

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -7,16 +7,22 @@ bauwerk.setup()
 
 
 def test_solar_battery_house():
+    """Basic test of solar battery house env."""
+
     env = gym.make("bauwerk/SolarBatteryHouse-v0", new_step_api=True)
     take_steps_in_env(env, num_steps=10)
 
 
 def test_build_dist_a():
+    """Basic test of building distribution A."""
+
     env = gym.make("bauwerk/BuildDistA-v0", new_step_api=True)
     take_steps_in_env(env, num_steps=10)
 
 
 def test_solar_battery_house_dict_config():
+    """Test the use of dict based config files."""
+
     ep_len = 24 * 4
     env = gym.make(
         "bauwerk/SolarBatteryHouse-v0", cfg={"episode_len": ep_len}, new_step_api=True
@@ -27,6 +33,7 @@ def test_solar_battery_house_dict_config():
 
 def test_env_seed():
     """Test of seed in distribution."""
+
     env1 = gym.make("bauwerk/BuildDistA-v0", seed=1)
     env2 = gym.make("bauwerk/BuildDistA-v0", seed=2)
     env3 = gym.make("bauwerk/BuildDistA-v0", seed=2)
@@ -34,7 +41,14 @@ def test_env_seed():
     assert env2.battery.size == env3.battery.size
 
 
-def take_steps_in_env(env, num_steps: int = 10) -> None:
+def take_steps_in_env(env: gym.Env, num_steps: int = 10) -> None:
+    """Take random steps in gym environment.
+
+    Args:
+        env (gym.Env): environment to take steps in.
+        num_steps (int, optional): number of steps to take. Defaults to 10.
+    """
+
     env.reset()
     for _ in range(num_steps):
         env.step(env.action_space.sample())
