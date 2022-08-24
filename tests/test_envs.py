edc53304091e61e931469512df5d8717dbe3205f
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-24 15:18:20 +0000

    test: add test for build dist A

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -3,16 +3,20 @@
 import bauwerk
 import gym
 
+bauwerk.setup()
+
 
 def test_solar_battery_house():
-    bauwerk.setup()
     env = gym.make("bauwerk/SolarBatteryHouse-v0", new_step_api=True)
     take_steps_in_env(env, num_steps=10)
 
 
-def test_solar_battery_house_dict_config():
-    bauwerk.setup()
+def test_build_dist_a():
+    env = gym.make("bauwerk/BuildDistA-v0", new_step_api=True)
+    take_steps_in_env(env, num_steps=10)
 
+
+def test_solar_battery_house_dict_config():
     ep_len = 24 * 4
     env = gym.make(
         "bauwerk/SolarBatteryHouse-v0", cfg={"episode_len": ep_len}, new_step_api=True
