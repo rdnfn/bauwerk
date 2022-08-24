44eddbbafff57b7a7d919951134d6abe75e77932
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-24 17:43:05 +0000

    feat: add support for seed in sampling

--- bauwerk/envs/distributions.py
+++ bauwerk/envs/distributions.py
@@ -4,7 +4,9 @@ import random
 import bauwerk.envs.solar_battery_house
 
 
-def sample_build_dist_a():
+def sample_build_dist_a(seed: int = None):
     """Sample env from building distribution A that varies battery size."""
+    if seed is not None:
+        random.seed(seed)
     sampled_cfg = {"battery_size": random.uniform(5, 15)}
     return bauwerk.envs.solar_battery_house.SolarBatteryHouseEnv(cfg=sampled_cfg)
