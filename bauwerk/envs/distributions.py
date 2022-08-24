21741b2983d3392d6e909b1543d94730ee57cb4d
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-24 15:21:08 +0000

    tidy: reduce lines in sampling code

--- bauwerk/envs/distributions.py
+++ bauwerk/envs/distributions.py
@@ -7,5 +7,4 @@ import bauwerk.envs.solar_battery_house
 def sample_build_dist_a():
     """Sample env from building distribution A that varies battery size."""
     sampled_cfg = {"battery_size": random.uniform(5, 15)}
-    env = bauwerk.envs.solar_battery_house.SolarBatteryHouseEnv(cfg=sampled_cfg)
-    return env
+    return bauwerk.envs.solar_battery_house.SolarBatteryHouseEnv(cfg=sampled_cfg)
