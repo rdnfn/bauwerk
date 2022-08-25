96f4ea422204a1db78dac9893907771bee2bb7e4
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-08-25 16:25:55 +0000

    fix: change log level setting in setup

--- bauwerk/core.py
+++ bauwerk/core.py
@@ -1,10 +1,14 @@
 """Main bauwerk module"""
 
-import os
+import sys
+from loguru import logger
 
 import bauwerk.envs.registration
 
 
 def setup(log_level="WARNING") -> None:
-    os.environ["LOGURU_LEVEL"] = log_level
+    # The line below would only work if done before loguru imported
+    # os.environ["LOGURU_LEVEL"] = log_level
+    logger.remove()
+    logger.add(sys.stderr, level=log_level)
     bauwerk.envs.registration.register_all()
