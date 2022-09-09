1bdaf502e2444281bed430b4f6f2e6d9d0e7ed18
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-09-09 10:53:16 +0000

    fix: change compat mode activation to anything below gym v0.26

--- bauwerk/constants.py
+++ bauwerk/constants.py
@@ -10,7 +10,7 @@ importlib_resources = bauwerk.utils.compat.get_importlib_resources()
 
 gym_version = version.parse(gym.__version__)
 
-GYM_COMPAT_MODE = gym_version >= version.parse("0.26")
+GYM_COMPAT_MODE = gym_version < version.parse("0.26")
 GYM_NEW_STEP_API_ACTIVE = gym_version >= version.parse("0.26")
 GYM_NEW_RESET_API_ACTIVE = gym_version >= version.parse("0.22")
 GYM_RESET_INFO_DEFAULT = gym_version >= version.parse("0.26")
