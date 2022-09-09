da455117ad379e6fb09f41696f1e30d41e521629
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-09-09 09:33:18 +0000

    refactor: rewrite gym checks

--- bauwerk/constants.py
+++ bauwerk/constants.py
@@ -8,8 +8,10 @@ import bauwerk.utils.compat
 
 importlib_resources = bauwerk.utils.compat.get_importlib_resources()
 
-NEW_STEP_API_ACTIVE = version.parse(gym.__version__) >= version.parse("0.25")
-NEW_RESET_API_ACTIVE = version.parse(gym.__version__) >= version.parse("0.22")
+gym_version = version.parse(gym.__version__)
+
+NEW_STEP_API_ACTIVE = gym_version >= version.parse("0.25")
+NEW_RESET_API_ACTIVE = gym_version >= version.parse("0.22")
 
 BAUWERK_DATA_PATH = importlib_resources.files("bauwerk.data")
 
