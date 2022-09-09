2efca2dc0fa20bbf67a69dfd6bd90a68e6d3b448
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-09-09 09:34:19 +0000

    fix: remove new api kwarg that breaks in gym 0.26

--- bauwerk/envs/registration.py
+++ bauwerk/envs/registration.py
@@ -2,30 +2,20 @@
 
 
 import gym.envs.registration
-from bauwerk.constants import NEW_STEP_API_ACTIVE
-
-# enable compatibility with older gym version
-if NEW_STEP_API_ACTIVE:
-    kwargs = {"new_step_api": True}
-else:
-    kwargs = {}
 
 
 def register_all() -> None:
     gym.envs.registration.register(
         id="bauwerk/SolarBatteryHouse-v0",
         entry_point="bauwerk.envs.solar_battery_house:SolarBatteryHouseEnv",
-        **kwargs,
     )
 
     gym.envs.registration.register(
         id="bauwerk/BuildDistA-v0",
         entry_point="bauwerk.envs.distributions:sample_build_dist_a",
-        **kwargs,
     )
 
     gym.envs.registration.register(
         id="bauwerk/BuildDistB-v0",
         entry_point="bauwerk.envs.distributions:sample_build_dist_b",
-        **kwargs,
     )
