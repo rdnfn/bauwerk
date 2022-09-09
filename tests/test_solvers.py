980e15d29ddb7a0213e60200df6d6505d1ad60ba
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-09-09 09:38:20 +0000

    test: update to prior changes in bauwerk api

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -8,6 +8,5 @@ import gym
 def test_solar_battery_house_solver() -> None:
     """Basic test whether running solver throws errors."""
 
-    bauwerk.setup()
     env = gym.make("bauwerk/SolarBatteryHouse-v0")
     bauwerk.envs.solvers.solve_solar_battery_house(env)
