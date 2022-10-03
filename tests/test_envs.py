4d6cfc3624baf2497f9b735a6d1e3b84477e7cb3
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-10-03 10:15:52 +0000

    test: update to new utils

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,6 +1,7 @@
 """Simple tests for environment."""
 
 import bauwerk  # pylint: disable=unused-import
+import bauwerk.utils.testing
 import gym
 import numpy as np
 import pytest
@@ -10,14 +11,14 @@ def test_solar_battery_house():
     """Basic test of solar battery house env."""
 
     env = gym.make("bauwerk/SolarBatteryHouse-v0")
-    take_steps_in_env(env, num_steps=env.cfg.episode_len)
+    bauwerk.utils.testing.take_steps_in_env(env, num_steps=env.cfg.episode_len)
 
 
 def test_build_dist_a():
     """Basic test of building distribution A."""
 
     env = gym.make("bauwerk/BuildDistA-v0")
-    take_steps_in_env(env, num_steps=10)
+    bauwerk.utils.testing.take_steps_in_env(env, num_steps=10)
 
 
 def test_solar_battery_house_dict_config():
@@ -26,7 +27,7 @@ def test_solar_battery_house_dict_config():
     ep_len = 24 * 4
     env = gym.make("bauwerk/SolarBatteryHouse-v0", cfg={"episode_len": ep_len})
     assert env.cfg.episode_len == ep_len
-    take_steps_in_env(env, num_steps=10)
+    bauwerk.utils.testing.take_steps_in_env(env, num_steps=10)
 
 
 def test_build_dist_b():
@@ -42,19 +43,6 @@ def test_build_dist_b():
     assert batt_size_1 != batt_size_2
     assert batt_size_2 == batt_size_3
     assert env.battery.size == env.cfg.battery_size
-
-
-def take_steps_in_env(env: gym.Env, num_steps: int = 10) -> None:
-    """Take random steps in gym environment.
-
-    Args:
-        env (gym.Env): environment to take steps in.
-        num_steps (int, optional): number of steps to take. Defaults to 10.
-    """
-
-    env.reset()
-    for _ in range(num_steps):
-        env.step(env.action_space.sample())
 
 
 def test_time_of_day():
