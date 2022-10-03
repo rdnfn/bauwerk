df134e65f9d3284d99e4ba0bb0227ddcd0890c5c
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-10-03 14:08:37 +0000

    feat: add support for normalised task param obs

--- bauwerk/envs/wrappers.py
+++ bauwerk/envs/wrappers.py
@@ -13,25 +13,39 @@ class TaskParamObs(gym.ObservationWrapper):
         task_param_names: list,
         task_param_low: np.array,
         task_param_high: np.array,
+        normalize=False,
     ):
         """Wrapper that adds task parameters to observation space."""
         super().__init__(env)
 
-        self.task_param_names = task_param_names
+        shape = (len(task_param_names),)  # shape of task param obs space
+        task_param_low = np.array(task_param_low).reshape(shape)
+        task_param_high = np.array(task_param_high).reshape(shape)
 
-        new_spaces = env.observation_space.spaces
-        new_spaces["task_param"] = gym.spaces.Box(
-            low=task_param_low,
-            high=task_param_high,
-            shape=(len(task_param_names),),
-            dtype=np.float32,
-        )
-        self.observation_space = gym.spaces.Dict(new_spaces)
+        self.task_param_names = task_param_names
 
         # get task parameter values
         self.task_param_values = np.array(
             [getattr(env.cfg, key) for key in task_param_names]
         )
+
+        if normalize:
+            self.task_param_values = [
+                (value - task_param_low[i]) / (task_param_high[i], task_param_low[i])
+                for i, value in enumerate(self.task_param_values)
+            ]
+            task_param_low = np.zeros(shape)
+            task_param_high = np.ones(shape)
+
+        new_spaces = env.observation_space.spaces  # new obs space starts from old
+        new_spaces["task_param"] = gym.spaces.Box(
+            low=task_param_low,
+            high=task_param_high,
+            shape=shape,
+            dtype=np.float32,
+        )
+
+        self.observation_space = gym.spaces.Dict(new_spaces)
 
     def observation(self, obs):
         obs["task_param"] = self.task_param_values
