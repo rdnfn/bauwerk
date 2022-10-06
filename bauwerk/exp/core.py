5efbaa2225964685002d658600ee8a14e95987bd
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-10-06 21:32:29 +0000

    feat: add wandb logging to exp script

--- bauwerk/exp/core.py
+++ bauwerk/exp/core.py
@@ -1,7 +1,7 @@
 """Utility functions to run experiments within Bauwerk."""
 
-from typing import Dict, Optional
-from dataclasses import dataclass, field
+from typing import Optional
+from dataclasses import dataclass
 from bauwerk.benchmarks import Task
 import bauwerk.benchmarks
 import bauwerk.envs.wrappers
@@ -12,6 +12,17 @@ import hydra
 from hydra.core.config_store import ConfigStore
 import stable_baselines3 as sb3
 from loguru import logger
+import wandb
+import wandb.integration.sb3
+import uuid
+
+
+@dataclass
+class Sb3Config:
+    """Kwargs for SB3 algorithm."""
+
+    policy: str = "MultiInputPolicy"
+    verbose: int = 0
 
 
 @dataclass
@@ -21,7 +32,7 @@ class ExpConfig:
     # env training params
     total_train_steps: int = 24 * 365  # total steps in env during training
     train_steps_per_task: int = 24 * 7 * 10
-    task_len: int = 24 * 30  # total length of task
+    task_len: int = 24  # * 30  # total length of task
 
     train_procedure: str = "consecutive"
     num_train_tasks: int = 5  # will sample
@@ -34,13 +45,14 @@ class ExpConfig:
 
     # algorithm params
     sb3_alg: str = "SAC"
-    sb3_alg_kwargs: Dict = field(default_factory=dict)
+    sb3_alg_kwargs: Sb3Config = Sb3Config()
 
     # evaluation
     eval_freq: int = 24 * 7  # evaluate model performance once per week
 
-    # logging
+    # logging & experiment tracking
     log_level: str = "INFO"
+    wandb_project: str = "bauwerk"
 
 
 cs = ConfigStore.instance()
@@ -49,7 +61,9 @@ cs.store(name="config", node=ExpConfig)
 
 @hydra.main(config_path=None, config_name="config", version_base=None)
 def run(cfg: ExpConfig):
-    """Run building distribution experiment."""
+    """Run Bauwerk building distribution experiment."""
+
+    run_id = uuid.uuid4().hex[:6]
 
     bauwerk.utils.logging.set_log_level(cfg.log_level)
 
@@ -75,11 +89,21 @@ def run(cfg: ExpConfig):
     elif cfg.train_procedure == "consecutive":
         tasks = build_dist.train_tasks[: cfg.num_train_tasks]
 
+    # setting up wandb logging
+    root_tensorboard_dir = f"outputs/sb3/runs/{run_id}"
+    wandb.tensorboard.patch(root_logdir=root_tensorboard_dir)
+    wandb_run = wandb.init(
+        project=cfg.wandb_project,
+        config=cfg,
+        sync_tensorboard=True,
+        id=run_id,
+    )
+
     if cfg.single_task is not None or cfg.train_procedure == "consecutive":
         model = model_cls(
-            policy="MultiInputPolicy",
             env=train_env,
-            verbose=0,
+            # tensorboard logs are necessary for full wandb logging
+            tensorboard_log=root_tensorboard_dir,
             **cfg.sb3_alg_kwargs,
         )
 
@@ -96,7 +120,6 @@ def run(cfg: ExpConfig):
 
         if cfg.train_procedure == "separate_models":
             model = model_cls(
-                policy="MultiInputPolicy",
                 env=train_env,
                 verbose=0,
                 **cfg.sb3_alg_kwargs,
@@ -107,9 +130,16 @@ def run(cfg: ExpConfig):
             eval_len=cfg.task_len,
             eval_freq=cfg.eval_freq,
         )
+        wandb_callback = wandb.integration.sb3.WandbCallback(
+            verbose=2,
+        )
         model.learn(
             total_timesteps=cfg.train_steps_per_task,
-            callback=[eval_callback],
+            callback=[eval_callback, wandb_callback],
+            # the last two configs prevent the log from being split up
+            # between learn calls
+            tb_log_name=f"run-{cfg.sb3_alg}-{wandb_run.id}",
+            reset_num_timesteps=False,
         )
 
         trained_models.append(model)
@@ -121,7 +151,7 @@ def run(cfg: ExpConfig):
             )
         )
 
-    # TODO: add wandb logging of results
+    wandb_run.finish()
 
 
 if __name__ == "__main__":
