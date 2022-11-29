482eaf327aa88d7ab522db6afb4e6039f232b9c6
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-11-29 16:37:42 +0000

    test: add test for experiment script

---
+++ tests/test_exp_script.py
@@ -0,0 +1,15 @@
+"""Test experiment script."""
+
+import bauwerk.exp.core
+from omegaconf import OmegaConf
+
+
+def test_default_run():
+    """Run experiment script with default experiment configuration."""
+
+    # Only difference to default is that wandb is deactivated to avoid sign-in issues.
+    # and reduced num of training steps for improved speed.
+    cfg = OmegaConf.create(
+        bauwerk.exp.core.ExpConfig(wandb_mode="disabled", train_steps_per_task=100)
+    )
+    bauwerk.exp.core.run(cfg)
