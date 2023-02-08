a8bb12dda0cbc361177a0ffd2c91d8de4160ae19
rdnfn <75615911+rdnfn@users.noreply.github.com>
2023-02-08 11:31:01 +0000

    test: update tests for plotting to new api

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -13,17 +13,13 @@ def test_core_plotting():
     env = gym.make("bauwerk/House-v0")
     initial_obs = bauwerk.utils.gym.force_old_reset(env.reset())
     _ = bauwerk.utils.plotting.EnvPlotter(
-        initial_obs=initial_obs,
         env=env,
         include_house_figure=True,
     )
     _ = bauwerk.utils.plotting.EnvPlotter(
-        initial_obs=initial_obs,
         env=env,
     )
-    _ = bauwerk.utils.plotting.EnvPlotter(
-        initial_obs=initial_obs, env=env, alternative_plotting=False
-    )
+    _ = bauwerk.utils.plotting.EnvPlotter(env=env, alternative_plotting=False)
 
 
 def test_opt_acts_plotting():
@@ -34,8 +30,18 @@ def test_opt_acts_plotting():
 
 def test_x_axis():
     env = gym.make("bauwerk/House-v0")
-    initial_obs = bauwerk.utils.gym.force_old_reset(env.reset())
-    plotter = bauwerk.utils.plotting.EnvPlotter(
-        initial_obs=initial_obs, env=env, alternative_plotting=True
-    )
+    env.reset()
+    plotter = bauwerk.utils.plotting.EnvPlotter(env=env, alternative_plotting=True)
     assert np.allclose(plotter.line_x[:4], np.array([0, 1, 2, 3]))
+
+
+def test_builtin_plotting():
+    # add render_mode to config to enable plotting
+    env = gym.make("bauwerk/House-v0", cfg={"render_mode": "rgb_array"})
+    initial_obs = env.reset()
+    for _ in range(24):
+        action = env.action_space.sample()
+        env.step(action)
+
+    env.render()
+    env.plotter.fig
